798c53d2f317c67b40beae2f359076dcf2411523
jasimuddin123 <jasimuddindxn@gmail.com>
2020-03-31 11:31:52 -0700

    let and const example

---
+++ letConst.js
@@ -0,0 +1,33 @@
+const hubby = "Jasim Uddin ";
+
+console.log(hubby);
+
+const numbers = [12 , 50];
+
+numbers[1] = 80;
+numbers.push(30);
+
+console.log(numbers);
+
+
+let lover = "Saim akter";
+
+lover = "Mother ";
+
+console.log(lover);
+
+// var can change  value
+let sum = 0;
+for(var i = 0; i < 10; i++ ){
+
+    sum = sum + i;
+}
+console.log(i);
+
+// let can't change the value
+
+// let sum = 0;
+// for(let i = 0; i < 10; i++){
+//     sum = sum + i;
+// }
+// console.log(sum);
